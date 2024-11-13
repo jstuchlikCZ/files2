95454093687b96d6e5b3ca2032b95d2d3dca54c9
jstuchlikCZ <HonzaStu@gmail.com>
2024-11-13 20:50:39 +0100

    Task 5 6 a 7

--- Lekce_8.py
+++ Lekce_8.py
@@ -101,4 +101,85 @@ with open (output_file_p3, "w") as output_file:
 print(f"Řádky ze souboru {source_file_p3} byly naklonováno do {output_file_p3} ve opačném pořadí")
 
 
+'''
+Task 5
+You have a text file. Calculate the number of words that begin with a character set by the user.
+'''
+source_file_task5 = 'C:/Users/Honza/Downloads/pes.txt'
+char = input("Vložte písmeno, kterým mají začínat slova v textu: ").lower()
+count = 0
+count2 = 0
+words_with_char = []
+
+with open(source_file_task5, "r", encoding="utf-8") as source_file:
+    for line in source_file:
+        words = line.split()
+        count += sum(1 for word in words if word.lower().startswith(char))
+        for word in words:
+            if word.lower().startswith(char):
+                words_with_char.append(word)
+                count2 += 1
+print(f"Počet slov začínajících na {char} je {count}")
+print(f"Jsou to tato slova: {words_with_char}")
+
+'''
+Task 6
+You have a text file. Write all its lines to another file while replacing * with & and vice versa.
+'''
+source_file_task6 = 'C:/Users/Honza/Downloads/pes.txt'
+output_file_task6 = 'C:/Users/Honza/OneDrive/Plocha/Python Kurz/Lekce 8 13.11.2024/Lekce8/klon_psa2.txt'
+
+
+with open(source_file_task6, "r", encoding="utf-8") as source_file6:
+    with open(output_file_task6, "w") as pes_klon2:
+        for line in source_file6:
+            modified_line = line.replace("*", "#TEMP#").replace("&", "*").replace("#TEMP#", "&")
+            pes_klon2.write(modified_line)
+
+'''
+Task 7
+You have an array of strings. 
+Write them to a file while placing each array element on a separate line and preserving the order.
+'''
+source_file_task7 = 'C:/Users/Honza/Downloads/pes_composed.txt'
+output_file_task7 = 'C:/Users/Honza/OneDrive/Plocha/Python Kurz/Lekce 8 13.11.2024/Lekce8/pes_decomposed.txt'
+
+with open(source_file_task7, "r", encoding="utf-8") as source_file7:
+    with open(output_file_task7, "w") as pes_decomposed:
+        for line in source_file7:
+            pes_decomposed.write(line + "\n")
+
+print(f"Řádky ze souboru {source_file_task7} byly naklonováno do {output_file_task7} ale rozdělen na řádky")
+'''
+Task 8
+You have an array of strings. 
+Write them to a file while placing each array element on a separate line and reversing the order.
+'''
+
+'''
+Task 9
+You have a text file. Calculate the number of characters in it.
+'''
+
+'''
+Task 10
+You have a text file. Calculate the number of lines in it.
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
